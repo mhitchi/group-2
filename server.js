1b6bb1f49616c7fe3051d9fa8ce0429b2a932220
mhitchi <mayhitchings@gmail.com>
2020-02-13 11:21:04 -0500

    added not nulls and tries in server.js

--- server.js
+++ server.js
@@ -8,6 +8,23 @@ const app = express();
 app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
 
+// // TRIED BASIC:
+// app.engine('.handlebars', exphbs({extname: '.handlebars'}));
+// app.set('view engine', '.handlebars');
+
+// TRIED path.join, threw ENOENT
+let viewsPath = path.resolve(__dirname, "/app", "/views")
+app.set('views', viewsPath);
+app.engine("handlebars", exphbs({ defaultLayout: "main", layoutsDir: viewsPath + '/layouts'})); 
+app.set("view engine", "handlebars");
+console.log(path.join(__dirname, "/app", "/views"))
+
+// Allows app to implement handlebars to template data
+// TRIED .handlebars, tried path.resolve, tried , extname: '.handlebars' 
+// app.engine(".handlebars", exphbs({ defaultLayout: "main", layoutsDir: viewsPath + '/layouts', extname: '.handlebars' })); 
+// app.set("view engine", ".handlebars");
+// console.log(path.resolve(__dirname, "/app", "/views"))
+
 //static directory
 app.use(express.static("./app/static"));
 
