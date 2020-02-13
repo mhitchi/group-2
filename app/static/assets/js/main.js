e2b030091ea8e04f9b336596c26262481d7515d4
mhitchi <mayhitchings@gmail.com>
2020-02-13 13:44:59 -0500

    started on click

--- app/static/assets/js/main.js
+++ app/static/assets/js/main.js
@@ -1,2 +1,23 @@
 //TODO ajax calls
 //TODO onbuttonclicks
+
+// get DOM elements
+const $viewBtn = $('.view-btn');
+const $addBtn = $('.add-btn');
+
+
+$viewBtn.on('click', function() {
+  //go to recipes page
+  $.ajax({
+    // TODO MAYBE DIFFERENT
+    url: '/api/recipes/all',
+    dataType: 'json',
+    type: 'GET'
+  }).then(function(response) {
+    
+  });
+});
+
+$addBtn.on('click', function() {
+  //go to forms page
+});
