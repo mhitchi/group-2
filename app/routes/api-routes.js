93723d7890d1348e483e028d103d575c947dd19e
MichaelRempe <michaeljrempe@gmail.com>
2020-02-13 12:08:15 -0500

    inital sequelize create method

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,5 +1,36 @@
-module.exports = function (app){
-  app.get('/', function(req, res){
-    res.render("index");
+//**********ROUTES DB CRUD OPs*************************************************************************/
+let db = require("../models");
+
+module.exports = function (app) {
+
+  //GET ALL RECIPES
+  app.get('/api/recipes/all', function (req, res) {
+    
+  });
+
+  //GET RECIPES BY MAIN ALC
+  app.get('/api/recipes/:main_alc', function (req, res) {
+    // res.render("index");
+  });
+
+  //POST NEW RECIPE
+  app.post("/api/recipes", (req, res) => {
+    console.log(req.body);
+    db.Todo.create({
+      name: req.body.whatever,
+      main_alc: req.body.whatever,
+      main_oz: req.body.whatever,
+      ing1: req.body.whatever,
+      ing2: req.body.whatever,
+      ing3: req.body.whatever,
+      ing4: req.body.whatever,
+      ing5: req.body.whatever,
+      ing6: req.body.whatever,
+      instructions: req.body.whatever
+
+    }).then((newRecipe) => {
+      console.log(newRecipe);
+      // res.json(dbTodo);
+    });
   });
 }
