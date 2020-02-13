f12a68f1f76f374d3d24314322613a8d08dc5b26
MichaelRempe <michaeljrempe@gmail.com>
2020-02-13 12:45:11 -0500

    File strucute ammended for DB folder

---
+++ app/db/seeds.sql
@@ -0,0 +1,4 @@
+USE recipes;
+INSERT INTO recipes(name, main_alc, ing1, instructions)
+VALUES("Jameson-Ginger", "whiskey", "ginger-ale", "Pour generous amount of Jameson mix in two drops of canada-dry. Drink.");
+
