3ae89631b636e88f517a26439db2a73cbb99534e
aya <aya.yoshida8@gmail.com>
2020-02-14 12:03:16 -0500

    input fields working

--- app/static/assets/js/main.js
+++ app/static/assets/js/main.js
@@ -14,15 +14,50 @@ $(document).ready(function(){
   const $instructions = $('#instructionsInput');
   const $addBtn = $('.add-recipe-btn');
   let alcName = "";
+  let newRecipe = {};
 
   $mainAlc.on('click', function() {
     alcName = this.id;
   });
 
-  $addBtn.on('click', function() {
-    //get main alc id
-    console.log('clicked add');
-    //get ingredient val
-    //post recipe to db
-  });
+  // $addBtn.on('click', function() {
+  //   //get main alc id
+  //   console.log('clicked add');
+  //   //get ingredient val
+  //   //post recipe to db
+  // });
+
+  $addBtn.on("click", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+    console.log("click");
+
+    let mainAlcohol = $(".alcohol option:selected").text();
+    console.log("You have selected" + mainAlcohol); 
+  
+    let newRecipe = {
+      name: $("#nameInput").val().trim(),
+      main_alc: mainAlcohol,
+      main_oz: $("#mainOz").val(),
+      ing1: $("#ing1").val(),
+      ing2: $("#ing2").val(),
+      ing3: $("#ing3").val(),
+      ing4: $("#ing4").val(),
+      ing5: $("#ing5").val(),
+      ing6: $("#ing6").val(),
+      instructions: $("#instructionsInput").val()
+    };
+    console.log(newRecipe);
+  })
+    // Send the POST request.
+    // $.ajax(“/api/form”, {
+    //   type: “POST”,
+    //   data: newRecipe
+    // }).then(
+    //   function() {
+    //     console.log(“created new recipe”);
+    //     // Reload the page to get the updated list
+    //     // location.reload();
+    //   }
+    // );
 })
