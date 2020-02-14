663a189d33fb3c65514743352563b199bee560ca
mhitchi <mayhitchings@gmail.com>
2020-02-14 10:12:51 -0500

    getting ingredients from DOM individually

--- app/static/assets/js/main.js
+++ app/static/assets/js/main.js
@@ -5,14 +5,17 @@ $(document).ready(function(){
   // get DOM elements
   const $name = $('#nameInput');
   const $mainAlc = $('.alcohol-item');
-  const $ingredients = $('.ingInput');
+  const $ing1 = $('#ing1');
+  const $ing2 = $('#ing2');
+  const $ing3 = $('#ing3');
+  const $ing4 = $('#ing4');
+  const $ing5 = $('#ing5');
+  const $ing6 = $('#ing6');
   const $instructions = $('#instructionsInput');
   const $addBtn = $('.add-btn');
 
-  const ingArr = [];
-
   $addBtn.on('click', function() {
-    //if ingredient val isnt empty, add to array
+    
     //post recipe to db
   });
 })
