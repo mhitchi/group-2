814f1e51f624abc34f9bb229ac88903c993ce40f
MichaelRempe <michaeljrempe@gmail.com>
2020-02-14 13:43:52 -0500

    find specifc recipe

--- app/static/assets/js/main.js
+++ app/static/assets/js/main.js
@@ -5,12 +5,6 @@ $(document).ready(function(){
   // get DOM elements
   const $name = $('#nameInput');
   const $mainAlc = $('.alcohol-item');
-  const $ing1 = $('#ing1');
-  const $ing2 = $('#ing2');
-  const $ing3 = $('#ing3');
-  const $ing4 = $('#ing4');
-  const $ing5 = $('#ing5');
-  const $ing6 = $('#ing6');
   const $instructions = $('#instructionsInput');
   const $addBtn = $('.add-recipe-btn');
   let alcName = "";
