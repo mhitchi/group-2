65f1d89d5c306fdf3564a27eb810708de4f3f59a
mhitchi <mayhitchings@gmail.com>
2020-02-14 10:06:04 -0500

    grabbing DOM elements into main.js

--- app/static/assets/js/main.js
+++ app/static/assets/js/main.js
@@ -3,23 +3,16 @@ $(document).ready(function(){
   //TODO onbuttonclicks
 
   // get DOM elements
-  const $viewBtn = $('.view-btn');
+  const $name = $('#nameInput');
+  const $mainAlc = $('.alcohol-item');
+  const $ingredients = $('.ingInput');
+  const $instructions = $('#instructionsInput');
   const $addBtn = $('.add-btn');
 
-  $viewBtn.on('click', function() {
-    // console.log("getting recipes");
-    // //go to recipes page
-    // $.ajax({
-    //   // TODO MAYBE DIFFERENT
-    //   // url: 'http://localhost:8080/recipes',
-    //   url: 'http://localhost:8080/recipes',
-    //   method: 'GET'
-    // }).then(function(response) {
-    //   console.log(response)
-    // });
-  });
+  const ingArr = [];
 
   $addBtn.on('click', function() {
-    //go to forms page
+    //if ingredient val isnt empty, add to array
+    //post recipe to db
   });
 })
