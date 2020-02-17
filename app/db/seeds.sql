774e46de990225ca8982890ec6ecfc706732d4d9
mhitchi <mayhitchings@gmail.com>
2020-02-17 12:38:52 -0500

    added to seeds

--- app/db/seeds.sql
+++ app/db/seeds.sql
@@ -2,3 +2,6 @@ USE recipes;
 INSERT INTO recipes(name, main_alc, ing1, instructions)
 VALUES("Jameson-Ginger", "whiskey", "ginger-ale", "Pour generous amount of Jameson mix in two drops of canada-dry. Drink.");
 
+INSERT INTO recipes(name, main_alc, main_oz, ing1, ing2, ing3, ing4, ing5, ing6, instructions)
+VALUES("Old Fashioned", "bourbon", 2, "1 tsp sugar", "2 dashes bitters", "1 orange slice", "twist of lemon peel", "1 marashino cherry", "Mix the sugar and bitters in an old fashioned glass with 2/4 oz water. Add bourbon and ice and stir. Garnish with orange slice, lemon peel and cherry.")
+
