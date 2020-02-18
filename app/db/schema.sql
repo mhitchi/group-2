85016101857ed41bb1cd85187c752a1550e13068
Ken Shiraishi <kshiraishi22@gmail.com>
2020-02-18 15:37:12 -0500

    added table to schema

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -1,2 +1,6 @@
-DELETE DATABASE IF EXISTS recipes;
-CREATE DATABASE recipes;
+-- DROP DATABASE IF EXISTS recipes;
+-- CREATE DATABASE recipes;
+
+USE o7vyjfac4fivejin;
+CREATE TABLE recipes(name VARCHAR(50), main_alc varchar(30), main_oz INTEGER(3), ing1 varchar(255), ing2 varchar(255), ing3 varchar(255), ing4 varchar(255), ing5 varchar(255), ing6 varchar(255), instructions TEXT
+);
